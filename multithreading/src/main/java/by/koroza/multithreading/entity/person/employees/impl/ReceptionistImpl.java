8a5204cad4b1b95208b31c5ed6a26e58b664d9ce
Alexey <96728779+Alexey-99@users.noreply.github.com>
2023-02-02 17:52:43 +0200

    Add files via upload

--- multithreading/src/main/java/by/koroza/multithreading/entity/person/employees/impl/ReceptionistImpl.java
+++ multithreading/src/main/java/by/koroza/multithreading/entity/person/employees/impl/ReceptionistImpl.java
@@ -1,5 +1,6 @@
 package by.koroza.multithreading.entity.person.employees.impl;
 
+import by.koroza.multithreading.entity.Computer;
 import by.koroza.multithreading.entity.abstraction.AbstractEmployees;
 import by.koroza.multithreading.entity.person.employees.Receptionist;
 import by.koroza.multithreading.entity.room.HookahRoom;
@@ -7,42 +8,42 @@ import by.koroza.multithreading.entity.room.HookahRoom;
 public class ReceptionistImpl extends AbstractEmployees implements Receptionist {
 
 	@Override
-	public boolean isHavingFreeHookahRoom() {
+	public boolean isHavingFreeHookahRoom(Computer computer) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public HookahRoom getFreeHookahRoom() {
+	public HookahRoom getFreeHookahRoom(Computer computer) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean isHavingFreePlacesInHookahRooms() {
+	public boolean isHavingFreePlacesInHookahRooms(Computer computer) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public HookahRoom getFreePlacesInHookahRoom() {
+	public HookahRoom getFreePlacesInHookahRoom(Computer computer) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
-	public boolean isHaveingFreeWaitingPlace() {
+	public boolean isHaveingFreeWaitingPlace(Computer computer) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	public void replacePersonToWaitingRoom() {
+	public void replacePersonToWaitingRoom(Computer computer) {
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void replacePersonToWaitingArea() {
+	public void replacePersonToWaitingArea(Computer computer) {
 		// TODO Auto-generated method stub
 	}
 
